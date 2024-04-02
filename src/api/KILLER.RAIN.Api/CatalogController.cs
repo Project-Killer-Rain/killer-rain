6d48426398863693d979a545873714a52baf73aa
Luis Gayola <gayolabarros.2@buckeyemail.osu.edu>
2024-04-02 10:20:58 -0400

    GetItem with Id

--- src/api/KILLER.RAIN.Api/CatalogController.cs
+++ src/api/KILLER.RAIN.Api/CatalogController.cs
@@ -21,6 +21,17 @@ namespace Onyx.Drum.Api.Controllers
             };
             return Ok(items);
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Item 1", "Description 1", "Brand 1", 100.00m)
+            {
+                Id = id
+            };
+
+            return Ok(item);
+        }
     }
 }
 
