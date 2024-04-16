62f24da109f8f9a9a3a4a50f2f97e9813a0553bb
Luis Gayola <gayolabarros.2@buckeyemail.osu.edu>
2024-04-16 15:38:58 -0400

    test api

---
+++ src/web/src/App.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen} from '@testing-library/react';
+import App from './App';
+
+describe("App component tests", () => {
+    test("should show title all the time", () =>{
+
+        render(<App/>)
+
+        expect(screen.getByText(/KILLER RAIN/i))
+        .toBeDefined()
+    })
+
+    test("should show 'All' link in the menu", () => {
+        render(<App/>)
+
+        expect(screen.getByText(/All/i))
+        .toBeDefined()
+    })
+})
